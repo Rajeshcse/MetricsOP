d9d30648dafc231f5da40d7d38f1cbf8740704c0
rparaman <imerajeshps@gmail.com>
2019-12-12 11:55:36 +0530

    initail commit

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds")
-    
+var connectDB = require("./config/db");
+connectDB();
+
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
+// mongodb+srv://rparaman:<password>@cluster0-owy2w.mongodb.net/test?retryWrites=true&w=majority
 //requiring routes
-var commentRoutes    = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index")
-    
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
-app.use(bodyParser.urlencoded({extended: true}));
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
+
+mongoose.connect("mongodb://localhost/yelp_camp_v10", { useMongoClient: true });
+// mongoose.connect(
+//   "mongodb+srv://rparaman:Geek123@cluster0-owy2w.mongodb.net/test?retryWrites=true",
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true
+//   }
+// );
+mongoose.Promise = global.Promise;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 // seedDB(); //seed the database
-
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The Metrics Server Has Started!");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
--- models/campground.js
+++ models/campground.js
@@ -1,48 +1,80 @@
 var mongoose = require("mongoose");
-
 var campgroundSchema = new mongoose.Schema({
-   name: String,
-   image: String,
-   description: String,
-   week: String,
-   month: String,
-   couts1:String,couts2:String,couts3:String,couts4:String,couts5:String,couts6:String,couts7:String,couts8:
-    String,couts9:String,couts10:String,couts11:String,couts12:String,
-    pv1:Number,
-    pv2:Number,
-    pv3:Number,
-    pv4:Number,
-    pv5:Number,
-    pv6:Number,
-    pv7:Number,
-    pv8:Number,
-    pv9:Number,
-    pv10: Number,
-    pv11: Number,
-    pv12:Number,
-    pv12:Number,pv13:Number,pv14:Number,pv15:Number,pv16:Number,pv17:Number,pv18:Number,pv19:Number,pv20:Number,pv21:Number,
-    pv22:Number,pv23:Number,pv24:Number,pv25:Number,pv26:Number,pv27:Number,pv28:Number,pv29:Number,pv30:Number,pv31:Number,
-    pv32:Number,pv33:Number,pv34:Number,pv35:Number,pv36:Number,pv37:Number,pv38:Number,pv39:Number,pv40:Number,pv41:Number,
-    pv42:Number,pv43:Number,pv44:Number,pv45:Number,
-    
-   created: { type :Date , default: Date },
-   
-    author: {
-      id: {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "User"
-      },
-      username: String
-   },
-   comments: [
-      {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "Comment"
-      }
-   ]
+  team: String,
+  couts1: String,
+  couts2: String,
+  couts3: String,
+  couts4: String,
+  couts5: String,
+  couts6: String,
+  couts7: String,
+  couts8: String,
+  couts9: String,
+  couts10: String,
+  couts11: String,
+  couts12: String,
+  pv1: Number,
+  pv2: Number,
+  pv3: Number,
+  pv4: Number,
+  pv5: Number,
+  pv6: Number,
+  pv7: Number,
+  pv8: Number,
+  pv9: Number,
+  pv10: Number,
+  pv11: Number,
+  pv12: Number,
+  pv12: Number,
+  pv13: Number,
+  pv14: Number,
+  pv15: Number,
+  pv16: Number,
+  pv17: Number,
+  pv18: Number,
+  pv19: Number,
+  pv20: Number,
+  pv21: Number,
+  pv22: Number,
+  pv23: Number,
+  pv24: Number,
+  pv25: Number,
+  pv26: Number,
+  pv27: Number,
+  pv28: Number,
+  pv29: Number,
+  pv30: Number,
+  pv31: Number,
+  pv32: Number,
+  pv33: Number,
+  pv34: Number,
+  pv35: Number,
+  pv36: Number,
+  pv37: Number,
+  pv38: Number,
+  pv39: Number,
+  pv40: Number,
+  pv41: Number,
+  pv42: Number,
+  pv43: Number,
+  pv44: Number,
+  pv45: Number,
+
+  created: { type: Date, default: Date },
+  week: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
-
-
-
